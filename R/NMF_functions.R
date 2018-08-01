bd373d442e46ebccc0e8937f7c86e3f0b623a97d
Frank Fineis <frank.fineis@gmail.com>
2018-07-31 19:36:00 -0500

    Update NMF_functions.R
    frank adds comments from Bin call 7/31/18

--- R/NMF_functions.R
+++ R/NMF_functions.R
@@ -1,6 +1,6 @@
 ####################################################################################
 # NMF functions
-# Non-negative matrix factorization
+# Non-negative matrix factorization with over-approximation.
 # input: coverage matrix (f) with columns representing samples
 # output: truncated SVD (svd), fitted coverage matrix (fitted), residuals (res), Lagrangian multipler (Lambda).
 NMF <- function(f, loop = 100){
@@ -61,12 +61,17 @@ optiNMF <- function(f, norm.factor){
     bin.num = dim(f.check)[2] %/% bin.size + 1
     bin.label = rep(c(1:bin.num), each = bin.size)
 
+    # decide whether to search for baseline regions of a gene.
+    # if max(ratio) < 0.8, skip baseline selection because there aren't samples with consistent degradation patterns.
+    # if gene.length < 200, and avg fragment length is 200, then reads aren't going to be reliable.
     if((min(ratio, na.rm = TRUE) > 0.9) || (max(ratio, na.rm = TRUE) < 0.8) || gene.length < 200){
       K = abs(NMF.output$svd$u) * NMF.output$svd$d[1]
       fitted = NMF.output$fitted
       fitted[f.check>fitted] = f.check[f.check>fitted]
     }else{
       
+      # Enter baseline selection loop.
+      # Continue while degradation is high.
       while(min(ratio) < 0.9){
         
         res.std = NMF.output$res / (f + 1)
@@ -93,11 +98,15 @@ optiNMF <- function(f, norm.factor){
         }
         ratio = rowSums(f) / (rowSums(NMF.output$fitted) + 1)
 
-        if((bin.num == 4) || (dim(f)[2] < 200)){
+        # "This step stops if the maximum DI score obtained from theremaining bins is <=0.1or 70% bins" - 
+        # The first criteria is cited from Online Methods; second criteria was imposed by Bin.
+        if((bin.num == 6) || (dim(f)[2] < 200)){
           break
         }
       }
       
+      # if we stopped during baseline selection and all samples are "kind of" degraded, just consider
+      # it that you HAVE found baseline. 
       if(min(ratio) > 0.8){
         
         output$convergence = TRUE
@@ -105,6 +114,9 @@ optiNMF <- function(f, norm.factor){
         G = apply(f.check, 2, function(x,K) max(x/K), K=K)
         fitted = K %*% G
         
+        # eliminate extreme cases where there's high degradation, but it's unlikely that
+        # the degradation comes from "degradation," degradation is just from noise. Prevents overinflating due to noise.
+        # Happens typically for larger genes with low sequencing depth.
         if(min(rowSums(f.check) / (rowSums(fitted)+1)) < 0.1){
           NMF.output = NMF(f.check)      
           K = abs(NMF.output$svd$u) * NMF.output$svd$d[1]
@@ -112,6 +124,7 @@ optiNMF <- function(f, norm.factor){
           fitted[f.check>fitted] = f.check[f.check>fitted]
         }
         
+      # If the gene didn't go through baseline selection, just use NMF-OA estimate.
       }else{
         NMF.output = NMF(f.check)      
         K = abs(NMF.output$svd$u) * NMF.output$svd$d[1]
@@ -130,6 +143,7 @@ optiNMF <- function(f, norm.factor){
   
 }
 
+# for intializing DegNorm iterations, use for obtaining sequencing depth factors.
 ratioSVD <- function(f, norm.factor){
   f = t(as.matrix(f))
   model_svd = svd(f, nv = 1, nu = 1)
